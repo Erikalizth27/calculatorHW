03724de7a48bb865a16b9f01a748c90e1fec78b8
Erikalizth27 <31712449+Erikalizth27@users.noreply.github.com>
2019-11-10 15:39:25 -0500

    Feature: created addition file and addition unit test

--- src/Calculator.py
+++ src/Calculator.py
@@ -2,9 +2,19 @@ from __future__ import division
 import math
 
 
+def addition(a, b):
+    a = int(a)
+    b = int(b)
+    c = b + a
+    return c
+
+
 class Calculator:
     result = 0
 
     def __init__(self):
         pass
 
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
