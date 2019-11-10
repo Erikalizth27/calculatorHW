aa2c2fe9f3fcbd6030894c04ea32e3240760f8c2
Erikalizth27 <31712449+Erikalizth27@users.noreply.github.com>
2019-11-10 15:46:10 -0500

    Feature: created multiplication file and multiplication unit test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -25,5 +25,11 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
+    def test_multiplication(self):
+        test_data = CsvReader('/src/multiplication.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.multiple(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
+
 if __name__ == '__main__':
     unittest.main()
