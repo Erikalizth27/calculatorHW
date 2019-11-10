71354d7cd164c877e819e116d673443c24b568b8
Erikalizth27 <31712449+Erikalizth27@users.noreply.github.com>
2019-11-10 15:44:02 -0500

    Feature: created subtraction file and subtraction unit test

--- src/Calculator.py
+++ src/Calculator.py
@@ -8,6 +8,12 @@ def addition(a, b):
     c = b + a
     return c
 
+def subtraction(a, b):
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
 
 class Calculator:
     result = 0
@@ -17,4 +23,8 @@ class Calculator:
 
     def add(self, a, b):
         self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
         return self.result
