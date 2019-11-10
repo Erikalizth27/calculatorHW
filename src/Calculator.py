db556be1287dcdb7b17c0146a534da394ec51d38
Erikalizth27 <31712449+Erikalizth27@users.noreply.github.com>
2019-11-10 15:36:32 -0500

    Task: created Calculator, calculatorTests and CsvReader files

---
+++ src/Calculator.py
@@ -0,0 +1,10 @@
+from __future__ import division
+import math
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
---
+++ src/CalculatorTests.py
@@ -0,0 +1,18 @@
+import unittest
+from Calculator import Calculator
+from CsvReader import CsvReader
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
